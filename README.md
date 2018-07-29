c63faaedd985521b45ea8ae22af2e995b2f840f5
OFFICERBROWN23 <41652622+OFFICERBROWN23@users.noreply.github.com>
2018-07-28 21:22:27 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-esx_treasurehunter
+# esx_poolcleaner
 
-Ocean Salvage Job for ESX
+Pool Cleaner Job for ESX
 
+# Getting Started
 
-Getting Started
+1. Add esx_poolcleaner folder into resources
+2. Add in server.cfg
+3. Add esx_poolcleaner.sql to database
 
-Add esx_treasurehunter folder into resources
+# Configuration
 
-Add in server.cfg
+1. Configuration only in config.lua
 
-Add esx_treasurehunter.sql to database
-
-Configuration
-
-Configuration only in config.lua
-
-Thanks to Anthony for esx_garbage, wich is the base of all of this. https://github.com/AnthonyEl
+Thanks to Anthony for esx_garbage, wich is the base of all of this.
+https://github.com/AnthonyEl
---
+++ server/main.lua
@@ -0,0 +1,78 @@
+ESX                			 = nil
+local PlayersVente			 = {}
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent('esx_treasurehunter:GiveItem')
+AddEventHandler('esx_treasurehunter:GiveItem', function()
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	local Quantity = xPlayer.getInventoryItem(Config.Zones.Vente.ItemRequires).count
+
+	if Quantity >= 15 then
+		TriggerClientEvent('esx:showNotification', _source, _U('stop_npc'))
+		return
+	else
+		local amount = Config.Zones.Vente.ItemAdd
+		local item = Config.Zones.Vente.ItemDb_name
+		xPlayer.addInventoryItem(item, amount)
+		TriggerClientEvent('esx:showNotification', _source, 'Salvage Collected ~g~ move on.')
+	end
+
+end)
+
+local function Vente(source)
+
+	SetTimeout(Config.Zones.Vente.ItemTime, function()
+
+		if PlayersVente[source] == true then
+
+			local _source = source
+			local xPlayer = ESX.GetPlayerFromId(_source)
+
+			local Quantity = xPlayer.getInventoryItem(Config.Zones.Vente.ItemRequires).count
+
+			if Quantity < Config.Zones.Vente.ItemRemove then
+				TriggerClientEvent('esx:showNotification', _source, '~r~You have no more bills to cash.')
+				PlayersVente[_source] = false
+			else
+				local amount = Config.Zones.Vente.ItemRemove
+				local item = Config.Zones.Vente.ItemRequires
+				Citizen.Wait(1500)
+				xPlayer.removeInventoryItem(item, amount)
+				xPlayer.addMoney(Config.Zones.Vente.ItemPrice)
+				TriggerClientEvent('esx:showNotification', _source, 'You have earned ~g~$' .. Config.Zones.Vente.ItemPrice)
+				Vente(_source)
+			end
+
+		end
+	end)
+end
+
+RegisterServerEvent('esx_treasurehunter:startVente')
+AddEventHandler('esx_treasurehunter:startVente', function()
+
+	local _source = source
+
+	if PlayersVente[_source] == false then
+		TriggerClientEvent('esx:showNotification', _source, '~r~No bills to cash!')
+		PlayersVente[_source] = false
+	else
+		PlayersVente[_source] = true
+		TriggerClientEvent('esx:showNotification', _source, '~g~Cashing ~w~bills...')
+		Vente(_source)
+	end
+end)
+
+RegisterServerEvent('esx_treasurehunter:stopVente')
+AddEventHandler('esx_treasurehunter:stopVente', function()
+
+	local _source = source
+
+	if PlayersVente[_source] == true then
+		PlayersVente[_source] = false
+	else
+		PlayersVente[_source] = true
+	end
+end)
